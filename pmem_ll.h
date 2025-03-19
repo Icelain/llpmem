3d8fa873e2d7d03d550454ae646f7811143f24a1
icelain <icelain@ice.computer>
2025-03-19 14:06:35 +0530

    add header for pmem linked list

---
+++ pmem_ll.h
@@ -0,0 +1,33 @@
+#ifndef PMEM_LOCK_FREE_LIST_H
+#define PMEM_LOCK_FREE_LIST_H
+
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct node {
+    int value;
+    _Atomic(struct node *) next;
+} PmemNode;
+
+PmemNode *createPmemNode(int value);
+
+static inline PmemNode *getNextPtrPmem(PmemNode *node);
+
+static inline bool isMarkedPmem(PmemNode *node);
+
+static inline PmemNode *getMarkedPtrPmem(PmemNode *node);
+
+void insertValuePmem(PmemNode *head, int value);
+
+PmemNode *findPmemNode(PmemNode *head, int value);
+
+bool deleteValuePmem(PmemNode *head, int value);
+
+int removeMarkedPmemNodes(PmemNode *head);
+
+void cleanupListPmem(PmemNode *head);
+
+void traversePmemNode(PmemNode *head);
+
+#endif /* PMEM_LOCK_FREE_LIST_H */
